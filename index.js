61b87715beddb9a6480506f48b8a2f61a03356c6
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2025-11-25 10:02:02 +0530

    create test project

---
+++ index.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.use((_, res, next) => {
+  const originalData = res.json.bind(res);
+  res.json = (data) => {
+    const newData = {
+      ...data,
+      team: {
+        frontend: "cool dev 1",
+        backend: "cool dev 2",
+      },
+    };
+    return originalData(newData);
+  };
+  next();
+});
+
+app.get("/", async (req, res) => {
+  const clientIpArray = req.ip.split(":");
+  const ip = clientIpArray[clientIpArray.length - 1];
+  fs.appendFile(
+    "./log.txt",
+    `Time: ${new Date().getDate()}, Client: ${ip}, Visited: ${req.path};\n`
+  );
+  res.json({
+    message: "Heyy",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Heyy");
+});
